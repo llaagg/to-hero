a6b9a387fdd19c0b2723d2b4b06b171f39116f98
Pawel Galezowski <pgalezowski@gmail.com>
2022-05-25 22:38:37 +0200

    fixes script

--- media/main.js
+++ media/main.js
@@ -8,13 +8,6 @@
         });
     });
 
-    document.querySelectorAll('.folder').forEach(function(e) {
-        e.addEventListener('click', (element) => {
-            var at = element.target.attributes['folderName'].value;
-            folderSelcted(at);
-        });
-    });
-
     document.querySelector('#new-project-button')
         .addEventListener('click', () => {
             callAction('newProject');
